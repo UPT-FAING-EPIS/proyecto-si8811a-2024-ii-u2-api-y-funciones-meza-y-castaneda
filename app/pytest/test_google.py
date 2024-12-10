f80974b7fc8643b29adc1634f2ab671eb2a8c009
mezamarco14 <102762929+mezamarco14@users.noreply.github.com>
2024-12-10 06:51:56 -0500

    v12

--- app/pytest/test_google.py
+++ app/pytest/test_google.py
@@ -4,7 +4,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../app')))
 from unittest.mock import patch, Mock
 from auth.google import google_login, google_authorized
-
+    
 # Mocks
 mock_config = Mock()
 mock_config.CLIENT_ID = 'test_client_id'
